0a25212f05e19b0933ff459c91e54c115b6b8206
软柿子 <gmh@hotmail.com>
2023-03-29 08:27:00 +0800

    firstPush

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { install } from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+install(Vue)
 
 Vue.config.productionTip = false
-
 new Vue({
   render: h => h(App),
 }).$mount('#app')
