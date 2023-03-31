d7abb9d8fa2cac4122f7da13fa26024139fca4a5
软柿子 <gmh@hotmail.com>
2023-03-31 08:38:36 +0800

    onetest

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,27 +1,16 @@
 <template>
-  <a-form-model layout="inline" :model="formInline"  @submit.native.prevent>
-    <a-form-model-item>
-      <a-input v-model="formInline.user" placeholder="Username">
-        <a-icon slot="prefix" type="user" style="color:rgba(0,0,0,.25)" />
-      </a-input>
-    </a-form-model-item>
-    <a-form-model-item>
-      <a-input v-model="formInline.password" type="password" placeholder="Password">
-        <a-icon slot="prefix" type="lock" style="color:rgba(0,0,0,.25)" />
-      </a-input>
-    </a-form-model-item>
-    <a-form-model-item>
-      <a-button
-        type="primary"
-        html-type="submit"
-        :disabled="formInline.user === '' || formInline.password === ''"
-      >
-        Log in
-      </a-button>
-    </a-form-model-item>
-  </a-form-model>
+  <div>
+    <a-form-model layout="inline" :model="formInline" @submit.native.prevent>
+      <a-form-model-item>
+        <a-range-picker v-model="date" @change="onChange" />
+      </a-form-model-item>
+    </a-form-model>
+    <button @click="fn">点击</button>
+  </div>
 </template>
 <script>
+// import moment from 'moment';
+import { quickTime } from '@/utlis/dayjs'
 export default {
   data() {
     return {
@@ -29,13 +18,38 @@ export default {
         user: '',
         password: '',
       },
-    };
+      date: quickTime('本季度'),
+    }
+  },
+  created() {
+    // console.log(quickTime('本年度'))
   },
   methods: {
-  },
-};
-</script>
+    onChange(value) {
+      console.log(value)
+    },
+    fn() {
+      this.fn1()
+    },
 
+    simpleDebounce(fn, delay = 100) {
+      let timer = null
+      return function () {
+        let args = arguments
+        if (timer) {
+          clearTimeout(timer)
+        }
+        timer = setTimeout(() => {
+          fn.apply(this, args)
+        }, delay)
+      }
+    },
+    fn1() {
+      console.log(123)
+    },
+  },
+}
+</script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
